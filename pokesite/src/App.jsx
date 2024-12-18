650063d3ce4ad4f748833f8faff16cafbd0811b9
c-garrett02 <cadegarrett2@gmail.com>
2024-12-18 17:16:42 -0500

    Working on transformations

--- pokesite/src/App.jsx
+++ pokesite/src/App.jsx
@@ -6,11 +6,11 @@ import venusaur from '/Venusaur.png'
 import './Temp.css'
 
 function App() {
+  console.log("rendering...")
   const [items, setItems] = useState([new Array(10).fill({index: 1, name: "bulbasaur", image:bulbasaur})])
   const [dexnum, setDexnum] = useState(0)
   const [name, setName] = useState(items[dexnum].name)
   const [image, setImage] = useState(items[dexnum].image)
-  const elementRef = useRef(null);
   const refArray = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)]
 
   useLayoutEffect(() => { //sets the items to the array of json objects, where each object represents 1 pokemon
@@ -23,13 +23,34 @@ function App() {
     fetchData();
   }, []);
 
-  const moveElement = () => {
+  const testMovement = () => {
     if (refArray[3].current) {
-      const margin = window.getComputedStyle(refArray[4].current).margin;
-      const zindex = window.getComputedStyle(refArray[4].current).zIndex;
-      refArray[3].current.style.zIndex = zindex;
+      const computedStyle = window.getComputedStyle(refArray[4].current)
+      const margin = computedStyle.margin;
+      const zIndex = computedStyle.zIndex;
+      const curPosition = refArray[3].current.getBoundingClientRect();
+      const nextPosition = refArray[4].current.getBoundingClientRect();
+
+      console.log((refArray[4].current.offsetTop - refArray[3].current.offsetTop) + 'px');
+      console.log(computedStyle.marginLeft);
+      refArray[3].current.style.top = '103px';
+      refArray[3].current.style.zIndex = zIndex;
     }
   };
+
+  function calculateTransformations() {  //Find difference between realtive top value, left margin, z index. Do this every time the window resizes, optimally.
+    const middle = Math.floor(refArray.length/2);
+    for (let i = middle; i < refArray.length - 1; i++){
+      const curElement = refArray[i].current;
+      const nextElement = refArray[i+1].current;
+      const curElementStyle = window.getComputedStyle(curElement);
+      const nextElementStyle = window.getComputedStyle(nextElement);
+
+      const topTransform = (nextElement.offsetTop - curElement.offsetTop) + 'px'; //Need to make into string so style can use it
+      const zTransform = nextElementStyle.zIndex - curElementStyle.zIndex;
+      const leftTransform = nextElementStyle.marginLeft - curElementStyle.marginLeft;
+    }
+  }
 
   function IncrementDex() { 
     if(dexnum < items.length - 1){
@@ -90,9 +111,9 @@ function App() {
       <div className='information'>
           <img className='pokeImage' src={image} />
       </div>
-      <div className='wheel' ref={elementRef}>
+      <div className='wheel'>
         <div className="decrementButton">
-          <button onClick={DecrementDex}>Decrement</button>
+          <button onClick={testMovement}>Decrement</button>
         </div>
         <VisibleEntries num={dexnum} />
         <div className="decrementButton">
