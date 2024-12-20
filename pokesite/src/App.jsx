2a3c34c19c59ce8926eea0f2615f7c7419ddd962
C-Garrett02 <cadegarrett2@gmail.com>
2024-12-19 22:30:12 -0500

    Got scrolling working, need to tweak it to make it safer for mashing.

--- pokesite/src/App.jsx
+++ pokesite/src/App.jsx
@@ -5,17 +5,18 @@ import venusaur from '/Venusaur.png'
 //import './App.css'
 import './Temp.css'
 
-const transformationArray = [];
+const forwardArr = [];
+const backwardsArr = [];
 
 function App() {
-  console.log("rendering...");
+  //console.log("rendering...");
   const [items, setItems] = useState([new Array(10).fill({index: 1, name: "bulbasaur", image:bulbasaur})]);
   const [dexnum, setDexnum] = useState(0);
   const [name, setName] = useState(items[dexnum].name);
   const [image, setImage] = useState(items[dexnum].image);
   const refArray = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)];
-  let animationStartTime = 0;
-  let AnimationStep = 1;
+  let animationStep = 1;
+  const totalAnimationSteps = 30;
   const intervalRef = useRef(null);
 
   useEffect(() => { //sets the items to the array of json objects, where each object represents 1 pokemon
@@ -31,80 +32,95 @@ function App() {
   useLayoutEffect(calculateTransformations, []); //LayoutEffect as we need to measure components after rendering.
 
   function calculateTransformations() {  //Find difference between realtive top value, left margin, z index. Do this every time the window resizes, optimally, so the logic doesn't have to rerun every render
-    console.log("calculating...")
-    transformationArray.length = 0;
-    const middle = Math.floor(refArray.length/2);
+    //console.log("calculating...")
+    forwardArr.length = 0;
     const matchStr = /\((\d*\.*\d*)\)/;
-    for (let i = middle; i < refArray.length - 1; i++){
+
+    for (let i = 0; i < refArray.length - 1; i++){ //For decrements
       const curElement = refArray[i].current;
       const nextElement = refArray[i+1].current;
       const curElementStyle = window.getComputedStyle(curElement);
       const nextElementStyle = window.getComputedStyle(nextElement);
 
-      const transformation = { //Values we'll have to add on to current values, gradually, to create a slide effect.
+      const transformation = { //Values we'll have to add on to current values, gradually, to create a slide effect. Everything but top will need a starting and ending reference.
         top: (nextElement.offsetTop - curElement.offsetTop), 
-        zIndex: nextElementStyle.zIndex - curElementStyle.zIndex,
-        marginLeft: parseInt(nextElementStyle.marginLeft) - parseInt(curElementStyle.marginLeft),
-        brightness: nextElementStyle.filter.match(matchStr)[1] - curElementStyle.filter.match(matchStr)[1]
+        marginLeft: {
+          start: parseInt(curElementStyle.marginLeft),
+          increment: parseInt(nextElementStyle.marginLeft) - parseInt(curElementStyle.marginLeft),
+          end: parseInt(nextElementStyle.marginLeft)
+        },
+        brightness: {
+          start: curElementStyle.filter.match(matchStr)[1],
+          increment: nextElementStyle.filter.match(matchStr)[1] - curElementStyle.filter.match(matchStr)[1],
+          end: nextElementStyle.filter.match(matchStr)[1]
+        }
       };
-
-      transformationArray.push(transformation)
+      forwardArr.push(transformation)
       console.log(transformation)
     }
-    //refArray[3].current.style.top = transformationArray[0].top + 'px';
-    //refArray[3].current.style.zIndex = +window.getComputedStyle(refArray[3].current).zIndex + +transformationArray[0].zIndex;
-    //refArray[3].current.style.marginLeft = transformationArray[0].marginLeft + 'px';
+
+    console.log('--------------')
+
+    for (let i = 1; i < refArray.length; i++){ //For increments
+      const curElement = refArray[i].current;
+      const nextElement = refArray[i-1].current;
+      const curElementStyle = window.getComputedStyle(curElement);
+      const nextElementStyle = window.getComputedStyle(nextElement);
+
+      const transformation = { //Values we'll have to add on to current values, gradually, to create a slide effect. Everything but top will need a starting and ending reference.
+        top: (nextElement.offsetTop - curElement.offsetTop), 
+        marginLeft: {
+          start: parseInt(curElementStyle.marginLeft),
+          increment: parseInt(nextElementStyle.marginLeft) - parseInt(curElementStyle.marginLeft),
+          end: parseInt(nextElementStyle.marginLeft)
+        },
+        brightness: {
+          start: curElementStyle.filter.match(matchStr)[1],
+          increment: nextElementStyle.filter.match(matchStr)[1] - curElementStyle.filter.match(matchStr)[1],
+          end: nextElementStyle.filter.match(matchStr)[1]
+        }
+      };
+      backwardsArr.push(transformation)
+      console.log(transformation)
+    }
+  
   }
 
-    /*const testMovement = () => {
-    if (refArray[3].current) {
-      const computedStyle = window.getComputedStyle(refArray[4].current);
-      const marginLeft = computedStyle.marginLeft;
-      const zIndex = computedStyle.zIndex;
-      const curPosition = refArray[3].current.getBoundingClientRect();
-      const nextPosition = refArray[4].current.getBoundingClientRect();
+  function slideEntriesUp() {
+    const multiplyBy = animationStep/totalAnimationSteps
 
-      console.log((refArray[4].current.offsetTop - refArray[3].current.offsetTop) + 'px');
-      console.log(computedStyle.marginLeft);
-      refArray[3].current.style.top = '103px';
-      refArray[3].current.style.zIndex = zIndex;
-      refArray[3].current.style.marginLeft = marginLeft;
+    for (let i = 0; i < refArray.length - 1; i++){ //loop through 
+      refArray[i].current.style.top = (+forwardArr[i].top)*multiplyBy + 'px'; 
+      refArray[i].current.style.marginLeft = +forwardArr[i].marginLeft.start + (+forwardArr[i].marginLeft.increment)*multiplyBy + 'px'; //offload the addition here to the calculation function in the future, perhaps.
+      refArray[i].current.style.filter = 'brightness(' + +(+forwardArr[i].brightness.start + (+forwardArr[i].brightness.increment)*multiplyBy) + ')';
     }
-  };*/
 
-  function slideEntryUp(entry) {
-    const timePassed = Date.now() - animationStartTime
-    console.log(timePassed);
-    let j = 3;
-
-    refArray[5].current.style.top = (transformationArray[1].top)/10*AnimationStep + 'px'; //NEEDS TO BE ADDED ONTO CURRENT NOT JUST ASSIGNED A FRACTION OF IT!!!!!!!
-    refArray[5].current.style.zIndex = (+window.getComputedStyle(refArray[5].current).zIndex + +transformationArray[1].zIndex)/10*AnimationStep;
-    refArray[5].current.style.marginLeft = (transformationArray[1].marginLeft/10)*AnimationStep + 'px';
-    refArray[5].current.style.filter = 'brightness(' + (transformationArray[1].brightness/10*AnimationStep) + ')'
-    console.log(refArray[5].current.style.marginLeft)
-
-    for (let i = Math.floor(refArray.length/2); i < refArray.length - 1; i++){
-      //refArray[i].current.style.top = (transformationArray[Math.abs(4-i)].top)/10*AnimationStep + 'px';
-      //refArray[i].current.style.zIndex = (+window.getComputedStyle(refArray[i].current).zIndex + +transformationArray[Math.abs(4-i)].zIndex)/10*AnimationStep;
-      //refArray[i].current.style.marginLeft = (transformationArray[Math.abs(4-i)].marginLeft/10)*AnimationStep + 'px';
-      //refArray[i].current.style.filter = 'brightness(' + (transformationArray[Math.abs(4-i)].brightness/10*AnimationStep) + ')'
-    }
-    //console.log(refArray[0])
-    //console.log(transformationArray[3])
-
-    j = 0;
-    AnimationStep++;
-    //console.log(transformationArray[0])
-
-    if(AnimationStep == 11){
-      AnimationStep = 1;
+    animationStep++;
+    if(animationStep > totalAnimationSteps - 1){ //last frame will be rendered
+      animationStep = 1;
       clearInterval(intervalRef.current);
+      DecrementDex();
     }
   }
 
-  function slideEntryDown(entry) {
-    const initialTime = Date.now();
-    console.log(Date.now()-initialTime);
+  function slideEntriesDown() {
+    const multiplyBy = animationStep/totalAnimationSteps
+    for (let i = 1; i < refArray.length; i++){ //loop through 
+      refArray[i].current.style.top = (+backwardsArr[i-1].top)*multiplyBy + 'px'; 
+      refArray[i].current.style.marginLeft = +backwardsArr[i-1].marginLeft.start + (+backwardsArr[i-1].marginLeft.increment)*multiplyBy + 'px'; //offload the addition here to the calculation function in the future, perhaps.
+      refArray[i].current.style.filter = 'brightness(' + +(+backwardsArr[i-1].brightness.start + (+backwardsArr[i-1].brightness.increment)*multiplyBy) + ')';
+    }
+
+    //refArray[0].current.style.top = (+backwardsArr[3].top)*multiplyBy + 'px'; 
+    //refArray[0].current.style.marginLeft = +backwardsArr[3].marginLeft.start + (+backwardsArr[3].marginLeft.increment)*multiplyBy + 'px'; //offload the addition here to the calculation function in the future, perhaps.
+    //refArray[0].current.style.filter = 'brightness(' + +(+backwardsArr[3].brightness.start + (+backwardsArr[3].brightness.increment)*multiplyBy) + ')';
+
+    animationStep++;
+    if(animationStep > totalAnimationSteps - 1){ //last frame will be rendered
+      animationStep = 1;
+      clearInterval(intervalRef.current);
+      IncrementDex();
+    }
   }
 
   function IncrementDex() { 
@@ -172,14 +188,17 @@ function App() {
         <div className='directionButtons'>
           <div className="decrementButton">
             <button onClick={() => {
-              //DecrementDex();
-
-              animationStartTime = Date.now();
-              intervalRef.current = setInterval(slideEntryUp, 10);
+              if(dexnum != 0){
+                intervalRef.current = setInterval(slideEntriesUp, 1000/60);
+              }
             }}>Decrement</button>
           </div>
           <div className="incrementButton">
-            <button onClick={IncrementDex}>Increment</button>
+            <button onClick={() => {
+              if(dexnum < items.length - 1){
+                intervalRef.current = setInterval(slideEntriesDown, 10);
+              }
+            }}>Increment</button>
           </div>
         </div>
         <VisibleEntries num={dexnum} />
