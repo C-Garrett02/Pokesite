179119751c5993193e845c9521ba96039f5a7785
C-Garrett02 <cadegarrett2@gmail.com>
2024-12-24 02:01:37 -0500

    Fancying up the bar chart

--- pokesite/src/StatsChart.jsx
+++ pokesite/src/StatsChart.jsx
@@ -4,37 +4,60 @@ import Chart from 'chart.js/auto';
 function StatsChart({stats}) {
 
     if(stats){
+      const colors = [];
+        for (const stat of Object.values(stats)){
+         if(stat <= 25){
+          colors.push('#ba0404')
+         }
+         else if (stat <= 60){
+          colors.push('#ed7207')
+         }
+         else if (stat <= 90){
+          colors.push('gold')
+         }
+         else if (stat <= 110){
+          colors.push('#8ded07')
+         }
+         else if (stat <= 130){
+          colors.push('#44ed07')
+         }
+         else{
+          colors.push('#06bd95')
+         }
+        }
         const data = {
             // Name of the variables on x-axies for each bar
-            labels: ["HP " + stats['hp'], "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"],
+            labels: [["HP", stats['hp']], ["Attack", stats['attack']], ["Defense", stats['defense']], ["Sp. Atk", stats['special-attack']], ["Sp. Def", stats['special-defense']], ["Speed", stats['speed']]],
             datasets: [
                 {
-                    // Data or value of your each variable
                     data: [stats['hp'], stats['attack'], stats['defense'], stats['special-attack'], stats['special-defense'], stats['speed']],
-                    // Color of each bar
-                    //backgroundColor: 
-                    //    ["aqua", "green", "red", "yellow"],
-                    // Border color of each bar
-                    //borderColor: ["aqua", "green", "red", "yellow"],
-                    //borderWidth: 0.5,
-                },
+                    backgroundColor: colors,
+                    //borderColor: 'black',
+                    //borderWidth: 2
+                }
             ],
         }
 
         const options = {
             indexAxis: 'y',
             maintainAspectRatio: false,
+            events: [],
             scales: {
               x: {
+                //display: false,
                 grid: {
                   display: false // This turns off the x-axis gridlines
                 },
+                max: 150
+                //ticks : {
+                //  display: false
+                //}
               },
               y: {
                 grid: {
                   display: false // This turns off the x-axis gridlines
-                },
-              },
+                }
+              }
             },
             plugins: {
                 legend: {
