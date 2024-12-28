442a88e15868e4e82ace365ee0975aac06c3bf99
c-garrett02 <cadegarrett2@gmail.com>
2024-12-28 17:25:34 -0500

    Changing abilities

--- pokesite/src/App.jsx
+++ pokesite/src/App.jsx
@@ -237,10 +237,40 @@ function App() {
 
   function Abilities(){
     const [abilityIndex, setAbilityIndex] = useState(0);
+    const [textEnd, setTextEnd] = useState("Ability 1");
+    function incrementAbility(){
+      if (abilityIndex+1 < abilities.length){
+        setAbilityIndex(abilityIndex+1);
+      }
+    }
+    function decrementAbility(){
+      if (abilityIndex-1 > -1){
+        setAbilityIndex(abilityIndex-1);
+      }
+    }
+    function generateTextEnd(){
+      if(abilities[abilityIndex].hidden){
+        return "(Hidden)";
+      }
+      else{
+        return "(Ability " + (abilityIndex + 1) + ")";
+      }
+    }
+
     return (
       <div className='abilityBox'>
-        <div className='abilityName'>
-          <b>{abilities[abilityIndex].name}</b>
+        <div className='abilityHeader'>
+          <div className='abilityName'>
+            <strong>{abilities[abilityIndex].name}</strong> <span>{textEnd}</span>
+          </div>
+          <div className='directionBtns'>
+            <button className='abilityBtn' onClick={decrementAbility}>
+              <img id='leftArrow' src='/triangle.svg' />
+            </button>
+            <button className='abilityBtn' onClick={incrementAbility}>
+              <img id='rightArrow' src='/triangle.svg' />
+            </button>
+          </div>
         </div>
         <div className='abilityDescription'>
           {abilities[abilityIndex].effect}
