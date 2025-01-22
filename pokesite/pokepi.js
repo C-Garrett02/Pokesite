8ad8977d8c812f4359c57c163244e4423a2b3302
c-garrett02 <cadegarrett2@gmail.com>
2025-01-22 11:37:29 -0500

    Enhancing move-getting algorithm

--- pokesite/pokepi.js
+++ pokesite/pokepi.js
@@ -7,56 +7,14 @@ async function GetPokemon2(dex) {
     return body.varieties;
 }
 
-async function GetMoves() { //write to separate file
-    const response = await fetch('https://pokeapi.co/api/v2/move?limit=100000&offset=0');
+async function GetVersions() { //get versions in chronological order
+    const response = await fetch('https://pokeapi.co/api/v2/version-group?limit=30');
     const body = await response.json();
-    let moves = [];
-    for (let move of body.results){
-        const move_response = await fetch(move.url);
-        const details = await move_response.json();
-        let description = "";
-        let move_name = "";
-        const move_key = move.name;
-        const damage_class = details.damage_class.name;
-        const move_accuracy = details.accuracy;
-        const move_power = details.power;
-        const move_pp = details.pp;
-        const move_priority = details.priority;
-        const move_target = details.target.name;
-        const move_type = details.type.name
-        for (let name of details.names) {
-            if(name.language.name == "en"){
-                move_name = name.name;
-            }
-        }
-        if(details.effect_entries.length > 0){
-            for (let entry of details.effect_entries) {
-                if(entry.language.name == "en"){
-                    description = entry.effect;
-                }
-            }
-        }
-        else {
-            for (let i = details.flavor_text_entries.length - 1; i >= 0; i--){ //Want to get the most recent entry, start from back
-                if(details.flavor_text_entries[i].language.name == "en"){
-                    description = details.flavor_text_entries[i].flavor_text;
-                }
-            }
-        }
-        moves.push({
-            key: move_key,
-            name: move_name,
-            class: damage_class,
-            accuracy: move_accuracy,
-            power: move_power,
-            pp: move_pp,
-            priority: move_priority,
-            target: move_target,
-            type: move_type,
-            effect: description
-        })
+    let versions = [];
+    for (let group of body.results) {
+        versions.push(group.name);
     }
-    return moves;
+    return versions;
 }
 
 async function GetAbility(ability) { //currently writes to pokedex, consider also making own file? Not as big of a deal as moves, though.
@@ -96,9 +54,11 @@ async function GetAbility(ability) { //currently writes to pokedex, consider als
 }
 
 async function GetFormData2(varieties) {
+    const versions = await GetVersions(); //Should really only call this once and pass it to this function or something. Will maybe work on that later.
     const response = await fetch(varieties[0].pokemon.url); //Will need to update this method, or another one, to deal with alternate types. Mega/gmax/regional/gender/etc
     const body = await response.json();
     const uppercaseName = body.name.charAt(0).toUpperCase() + body.name.slice(1);
+    let latestIndex = 0;
     let base_stats = {};
     let type_array = [];
     let ability_list = [];
@@ -130,9 +90,16 @@ async function GetFormData2(varieties) {
         currentAbilityDescription.hidden = ability.is_hidden;
         ability_list.push(currentAbilityDescription);
     }
+    for (let move of body.moves){ //iterates through moves an extra time up front to determine which generation has the latest moveset
+        let arrayLength = move.version_group_details.length;
+        let moveIndex = versions.indexOf(move.version_group_details[arrayLength - 1].version_group.name);
+        if (moveIndex > latestIndex){
+            latestIndex = moveIndex;
+        }
+    }
     for (let move of body.moves){
         for (let version of move.version_group_details){
-            if (version.version_group.name = "scarlet-violet"){
+            if (version.version_group.name == versions[latestIndex]){
                 let current_move = {};
                 current_move.key = move.move.name;
                 current_move.level = version.level_learned_at;
@@ -180,7 +147,6 @@ async function GetFormData2(varieties) {
 async function PushPokemon(dex) {
     const varieties = await GetPokemon2(dex);
     const pokemon = await GetFormData2(varieties);
-    //console.log(pokemon)
     pokedex.push(pokemon)
 }
 
@@ -202,10 +168,6 @@ let pokedex = [];
 for (let i = 1; i <= 151; i++) {
     promises.push(PushPokemon(i));
 }
-
-const moves = await GetMoves();
-
-SaveToFile(moves, 'moves.json');
 
 Promise.all(promises)
     .then(() => {
