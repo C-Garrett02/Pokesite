7a41909f038e3978aabb178613979b9a9c01f878
C-Garrett02 <cadegarrett2@gmail.com>
2025-02-04 23:27:52 -0500

    Basically copied down evolution-chain responses into a json file

--- pokesite/lineget.js
+++ pokesite/lineget.js
@@ -13,8 +13,8 @@ function SaveToFile(array, filename) {
         
 }
 
-async function GetChain(id) {
-    const response = await fetch('https://pokeapi.co/api/v2/evolution-chain/'+id);
+async function RawToJSON(url) {
+    const response = await fetch(url);
     const body = await response.json();
     return body;
 }
@@ -22,8 +22,17 @@ async function GetChain(id) {
 async function GetEvoLines() { //write to separate file
     const response = await fetch('https://pokeapi.co/api/v2/evolution-chain?limit=100000&offset=0');
     const body = await response.json();
+    const chains = [];
+    for (let obj of body.results){
+        const chain = await RawToJSON(obj.url);
+        chains.push(chain);
+    }
+    return chains;
 }
 
 const evo_lines = await GetEvoLines();
+
+//Will have to now insert code to merge in some other handwritten json file to account for forms
+//Algorithmically treating certain forms like a "species" has too many variables and there's no way I can think of to account for every scenario
 
 SaveToFile(evo_lines, 'evolutions.json');
