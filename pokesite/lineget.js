f09310b941c133345de82ce02b5277a15b5451de
c-garrett02 <cadegarrett2@gmail.com>
2025-02-27 14:53:56 -0500

    Creating an array of conditions

--- pokesite/lineget.js
+++ pokesite/lineget.js
@@ -1,4 +1,6 @@
 import { createRequire } from 'module';
+import { readFile } from 'fs/promises';
+
 const require = createRequire(import.meta.url);
 
 function SaveToFile(array, filename) {
@@ -19,20 +21,124 @@ async function RawToJSON(url) {
     return body;
 }
 
+async function GetItems() { //write to separate file
+    let body = JSON.parse(await readFile("./public/items.json", "utf8"));
+    return body;
+}
+
+const items = await GetItems();
+
+async function GetMoves() { //write to separate file
+    let body = JSON.parse(await readFile("./public/moves.json", "utf8"));
+    return body;
+}
+
+const moves = await GetMoves();
+
+function RetrieveItemName(key) {
+    for(let item of items){
+        if(item.key == key) {
+            return item.name;
+        }
+    }
+}
+
+function RetrieveMoveName(key) {
+    for(let move of moves){
+        if(move.key == key) {
+            return move.name;
+        }
+    }
+}
+
+//code goes here for creating some sort of "evolution string" that notes the conditions. Maybe even an array of strings? ["Level", "Knows Move", "Female"] for ex
+function GetEvoStringArray(details) {
+    const strArray = [];
+    if (details.gender != null) {
+        strArray.push(details.gender == 2 ? "Male" : "Female");
+    }
+    if (details.held_item != null) {
+        strArray.push("While Holding" + RetrieveItemName(details.held_item.name));
+    }
+    if (details.item != null) {
+        strArray.push(RetrieveItemName(details.item.name));
+    }
+    if (details.known_move != null) {
+        strArray.push(RetrieveMoveName(details.known_move.name));
+    }
+    if (details.known_move_type != null) {
+        //Usually avoid the uppercase hack but it will work for types
+        let type = details.known_move_type.name[0].toUpperCase() +  details.known_move_type.name.slice(1);
+        strArray.push("Knows a " + type + " move");
+    }
+
+    //skipping location, probably not needed for anyone as of gen 9. Will probably have to handwrite ones like runerigus that involve a location anyways.
+    //skippng affection
+    //skipping beauty
+
+    if (details.min_level != null) {
+        strArray.push("Lvl " + details.min_level);
+    }
+    if (details.needs_overworld_rain == true) {
+        strArray.push("Rain");
+    }
+    if (details.party_species != null) {
+        strArray.push("Remoraid in party"); //hardcoded for mantine
+    }
+    if (details.party_type != null) {
+        strArray.push("Dark type in party"); //hardcoded for pangoro
+    }
+    if (details.relative_physical_stats != null) {
+        let hitmonArray = ["Attack < Defense", "Attack = Defense", "Attack > Defense"]
+        strArray.push(hitmonArray[details.relative_physical_stats + 1]); //hardcoded for hitmon line
+    }
+
+    //Time of day (TOD) conditions
+    if (details.time_of_day == "day") {
+        strArray.push("Day"); 
+    }
+    if (details.time_of_day == "night") {
+        strArray.push("Night"); 
+    }
+    if (details.time_of_day == "dusk") {
+        strArray.push("Dusk"); 
+    }
+    if (details.time_of_day == "full-moon") {
+        strArray.push("Full Moon"); 
+    }
+
+    if (details.trade_species != null) {
+        //Usually avoid the uppercase hack but it will work for these 2 pokemon
+        strArray.push(details.trade_species.name[0].toUpperCase() +  details.trade_species.name.slice(1)); 
+    }    
+    if (details.turn_upside_down == true) {
+        strArray.push("Upside Down")
+    }
+
+    return strArray;
+}
+
+function StringifyAllDetails(chain) {
+    for (let evo of chain.evolves_to){
+        for (let details of evo.evolution_details){
+            const evoStringArr = GetEvoStringArray(details);
+            details.conditionArray = evoStringArr;
+        }
+        StringifyAllDetails(evo);
+    }
+}
+
 async function GetEvoLines() { //write to separate file
     const response = await fetch('https://pokeapi.co/api/v2/evolution-chain?limit=100000&offset=0');
     const body = await response.json();
     const chains = [];
     for (let obj of body.results){
         const chain = await RawToJSON(obj.url);
+        StringifyAllDetails(chain.chain);
         chains.push(chain);
     }
     return chains;
 }
-
-//code goes here for creating some sort of "evolution string" that notes the conditions. Maybe even an array of strings? ["Level", "Knows Move", "Female"] for ex
-//may have to get/utilize all the types/items/moves to make this thing dynamic/neat. Types I can probably hack and just capitalize the first letter of. Items have a few unique cases that -
-// - would make that less than ideal. Dashes in between words, maybe some apostrophes. We should probably do the same with moves to be safe, but I do have a moves json
 
 const evo_lines = await GetEvoLines();
 
